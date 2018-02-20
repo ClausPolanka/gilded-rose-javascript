fce61d0d650153e8bdc700ce63ac3762eb471aae
Kraus Milan <milan.kraus@vacuumlabs.com>
2018-02-20 13:42:43 +0100

    Added unit test - regular item

--- src/__tests__/gilded_rose_spec.js
+++ src/__tests__/gilded_rose_spec.js
@@ -2,10 +2,32 @@ import { Shop, Item } from '../gilded_rose';
 
 describe("Gilded Rose", function () {
 
-    it("should foo", function () {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
+    describe("Regular item", function() {
+        it("should decrease quality by one", function () {
+            const gildedRose = new Shop([new Item("Regular Item", 10, 5)]);
+            const items1Day = gildedRose.updateQuality();
+            expect(items1Day[0].quality).toEqual(4);
+            const items2Days = gildedRose.updateQuality();
+            expect(items1Day[0].quality).toEqual(3);
+        });
+
+        it("should not decrease quality below zero", function() {
+            const gildedRose = new Shop([new Item("Regular Item", 10, 1)]);
+            const items1Day = gildedRose.updateQuality();
+            expect(items1Day[0].quality).toEqual(0);
+            const items2Days = gildedRose.updateQuality();
+            expect(items1Day[0].quality).toEqual(0);
+        });
+
+        it("should decrease quality by two after sell-in day", function() {
+            const gildedRose = new Shop([new Item("Regular Item", 1, 10)]);
+            const items1Day = gildedRose.updateQuality();
+            expect(items1Day[0].quality).toEqual(9);
+            const items2Days = gildedRose.updateQuality();
+            expect(items1Day[0].quality).toEqual(7);
+            const items3Days = gildedRose.updateQuality();
+            expect(items1Day[0].quality).toEqual(5);
+        });
     });
 
 });
